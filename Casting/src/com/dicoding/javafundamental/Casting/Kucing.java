df98c3eb77572135576c2ea1d4f8e0babaec5545
ayufebriani <73220887+ayufebriani@users.noreply.github.com>
2021-01-09 20:27:51 +0700

    Add files via upload

---
+++ Casting/src/com/dicoding/javafundamental/Casting/Kucing.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dicoding.javafundamental.Casting;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Kucing extends Hewan {
+     public void makan() {
+        // ...
+    }
+ 
+    public void meow() {
+        // ...
+    }
+}
+    
